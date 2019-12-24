26e829dc8a987124a8a3d1ab1811fe83226b9c11
iiiiiiii98 <58320580+iiiiiiii98@users.noreply.github.com>
2019-12-24 14:10:31 +0900

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,95 @@
+// data.js
+
+/** 제품 정보 데이터(JSON) */
+var product_data = [
+  {
+    // 속성: 값
+    img_url: "./images/p1.jpg",
+    title: "malling booth",
+    name: "(만년형) 클리어 다이어리 커버",
+    origin_price: "12,900",
+    sale_price: "8,500",
+    category: "woman",
+  },
+  {
+    img_url: "./images/p3.jpg",
+    title: "malling booth",
+    name: "ZIPPER POUCH DIARY_CLEAR",
+    origin_price: "",
+    sale_price: "12,000",
+    category: "woman",
+  },
+  {
+    img_url: "./images/p2.jpg",
+    title: "ARDIUM",
+    name: "아르디움 블루네임텍",
+    origin_price: "3,700원",
+    sale_price: "12,00",
+    category: "woman",
+  },
+  {
+    img_url: "./images/p4.jpg",
+    title: "twopigs",
+    name: "폭닥폭닥 인조양모 다이어리커버",
+    origin_price: "",
+    sale_price: "23,500",
+    category: "woman",
+  },
+  {
+    img_url: "./images/p11.jpg",
+    title: "twopigs",
+    name: "2020 레드캘린더",
+    origin_price: "8,000원",
+    sale_price: "7,800",
+    category: "man",
+  },
+  {
+    img_url: "./images/p7.jpg",
+    title: "partypang",
+    name: "신년 파티장식세트 [헬로 2020 골드블랙]",
+    origin_price: "32,000원",
+    sale_price: "27,200",
+    category: "man",
+  },
+  {
+    img_url: "./images/p8.jpg",
+    title: "partypang",
+    name: "신년 파티장식세트[헬로2020 로즈골드]",
+    origin_price: "37,000원",
+    sale_price: "31,400",
+    category: "man",
+  },
+  {
+    img_url: "./images/p6.jpg",
+    title: "decobonbon",
+    name: "[데코봉봉]메리크리스마스 가랜드",
+    origin_price: "5,900원",
+    sale_price: "5,010",
+    category: "monster",
+  },
+  {
+    img_url: "./images/p5.jpg",
+    title: "Theflowermarket",
+    name: "올리브가랜드 + LED 에디슨 전구 풀세트",
+    origin_price: "",
+    sale_price: "16,800",
+    category: "man",
+  },  
+  {
+    img_url: "./images/p9.jpg",
+    title: "월트디즈니",
+    name: "빨간리본 스누피",
+    origin_price: "12,000원",
+    sale_price: "11,850",
+    category: "woman",
+  },  
+  {
+    img_url: "./images/p10.jpg",
+    title: "더클락",
+    name: "뭉게뭉게 아날로그 시계",
+    origin_price: "18,900원",
+    sale_price: "17,750",
+    category: "man",
+  },  
+];
+
